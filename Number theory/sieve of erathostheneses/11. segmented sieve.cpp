f12c78b2d741eed284555d31949b37548ef6716b
Dipraj Daripa <62438853+dipraj14@users.noreply.github.com>
2022-08-26 10:21:52 +0530

    Update 11. segmented sieve.cpp

--- Number theory/sieve of erathostheneses/11. segmented sieve.cpp
+++ Number theory/sieve of erathostheneses/11. segmented sieve.cpp
@@ -1,116 +1,10 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+// Segmented Sieve Algo
 
-// void segmented_sieve(){
- 
-//         int l;
-//         int r;
-//         cin >> l >> r;
-
-//         // 
-     
-//         int N = sqrt(r);
-
-//         bool sieve[N+1];
-
-//         for(int i=1; i<= N;i++){
-//             sieve[i] = true;
-//         }
-
-//         for(int i =2; i*i<=N ;i++){
-//             if(sieve[i] == true){
-//                 for(int j=i*i;j<=N;j+=i){
-//                     sieve[j] = false;
-//                 }
-//             }
-//         }
-//        // step1: generate prime till sqrt(n)
-//         vector<int> prime;
-
-//         for(int i=2;i<=N;i++){
-//             if(sieve[i] == true){
-//                 prime.push_back( i);
-//             }
-//         }
-//         /*for(auto x: prime){
-//             cout<< x<<" ";
-//         }
-//         */
-//         int size = r-l;
-//         bool mark[size];
-    
-//         for(int i=0;i<=size;i++){
-//             mark[i] = true;
-//         }
-        
-//         // sptep 2: mark all prime multiple given l to r
-//         for(auto pr: prime){
-
-//             int firstmiltipale = (l/pr)*pr;
-//             if(firstmiltipale < l){
-//                 firstmiltipale+=pr;
-//             }
-            
-            
-//             // in case of 11 you will get firstmiltipale is 110 but you need to take 11*11
-//             for(int i=max(pr*pr, firstmiltipale) ;i<=r; i+=pr){
-//                 // 110 - 0   (110 -110)
-//                 // 111 = 1   (111-110)
-//                 // 112 - 2  (112 - 110)
-//                 mark[i-l] = false;
-
-//             }
-          
-//         }
-        
-         
-//         /*for(int i=0;i<=size;i++){
-//             cout<< mark[i]<<" ";
-//         }*/
-        
-//        /*countprime between l to r
-//        int cnt = 0;
-        
-//         for(int i=l;i<=r ;i++){
-//             if(mark[i-l] == 1){
-//                 cnt++;
-//             }
-//         }
-        
-//         cout << cnt <<endl;
-//         */
-//        // print prime between l to r
-//        vector<int> ans;
-//        for(int i=l;i<=r ;i++){
-//             if(mark[i-l] == 1){
-//                 ans.push_back(l+(i-l));
-//             }
-//         }
-//         for(auto x: ans){
-//             cout<< x<< " ";
-//         }
-        
-
-
-   
-// }
-
-// int main(){
-    
-//    segmented_sieve();
-
-// }
-
-
-// NEWLY WRITTEN CODE 
-
+// QUESTION -> // Print all the Prime number Between L - R Segmrnt 
 
 // T <= 10
 // 1 <= (L, R) <= 10^12  
 // R - L <= 10^6          // highest --> 10^ 6
-
-
-// Print all the Prime number Between L - R Segmrnt 
 
 #include <bits/stdc++.h>  // this will work only for 10^6 but pefixPrime() which was work upto 10^7 ----> impleting below
 
@@ -216,13 +110,17 @@ int main()
 
 
 
-// Smae Question But better than Segmented Sive Concept and Easy to remember  ---> name ---> prefixPrime() -> 
+// Smae Question But better than Segmented Sive Concept and Easy to remember  ---> name ---> prefixPrime() ->
 // 1 ≤ T ≤ 10^5
 // 1 ≤ L ≤ 10^7
 // 1 ≤ R ≤ 10^7   // this gives TLE if we implemented with Sigmented Sieve Algo 
 
 // https://www.codechef.com/problems/CNTPRIME?tab=statement --> question ---> if we submite using Segment Sive Algo than it give TLE and Partilly Correct Ans
 // BUT if we Submit USing PrefixPrime() is give correct ans 
+
+// BUT PrefixPrime() uses only for count purpose --> not for Printing purpose
+// Printing and count purpose -> Segmented Sieve algo (Both work)
+
 
 #include <bits/stdc++.h>
 using namespace std;
